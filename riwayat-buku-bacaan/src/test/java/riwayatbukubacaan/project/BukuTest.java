9dd82acaa6d0c66f0dbd32e3c523ea69c8b448e1
hanlie <johansama9999@gmail.com>
2024-07-14 13:50:11 +0700

    [UPDATE] mengganti nama file Buku.class -> BukuTest.class

--- riwayat-buku-bacaan/src/test/java/riwayatbukubacaan/project/BukuTest.java
+++ riwayat-buku-bacaan/src/test/java/riwayatbukubacaan/project/BukuTest.java
@@ -1,14 +1,17 @@
 package riwayatbukubacaan.project;
 
+import org.junit.jupiter.api.Test;
+
 import java.sql.Timestamp;
 
-public class Buku {
+
+public class BukuTest {
     private int no;
     private String namaBuku;
     private String penulis;
     private Timestamp tanggalDitambahkan;
 
-    public Buku (int no, String namaBuku, String penulis, Timestamp tanggalDitambahkan) {
+    public BukuTest (int no, String namaBuku, String penulis, Timestamp tanggalDitambahkan) {
         this.no = no;
         this.namaBuku = namaBuku;
         this.penulis = penulis;
